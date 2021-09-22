fe7d2dfc9cbf6a4cb21c2310ce0b13d0b8483f7c
Edward Pickup <Edward.Pickup@arm.com>
2021-09-22 14:44:44 +0100

    arm/pci: Add pci-scan boot argument
    This patch adds a Xen boot arguments that, if enabled, causes a call to
    existing code to scan pci devices enumerated by the firmware.
    
    This patch also makes an existing debug function viewable outside its
    translation unit, and uses this to dump the PCI devices found.
    The debug message is controlled by an additional configurable added to
    arch/arm/Kconfig.debug.
    
    Additionally, this patch modifies segment loading to ensure that PCI
    devices on other segments are properly found.
    
    This will be needed ahead of dom0less support for pci passthrough on
    arm.
    
    Issue-Id: SCM-1368
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Signed-off-by: Edward Pickup <Edward.Pickup@arm.com>
    Change-Id: I79eb1325019fb37fe0c7951070efe5745fed6905
    ---
    (cherry picked from commit bce463e1588a45e1bfdf59fc0d5f88b16604e439 from
     the downstream branch poc/pci-passthrough from
     https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc.git)
    
    changes since cherry-pick:
    * s/always_inline/inline/
    * fixup debug Kconfig option s/CONFIG_//
    * rebase on ("Revert "xen/arm: Add cmdline boot option "pci-passthrough = <boolean>""")
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>

--- xen/arch/arm/pci/pci.c
+++ xen/arch/arm/pci/pci.c
@@ -84,6 +84,9 @@ bool arch_pci_device_physdevop(void)
 /* By default pci passthrough is disabled. */
 bool __read_mostly pci_passthrough_enabled;
 boolean_param("pci-passthrough", pci_passthrough_enabled);
+/* By default pci scan is disabled. */
+bool __read_mostly pci_scan_enabled;
+boolean_param("pci-scan", pci_scan_enabled);
 
 static int __init pci_init(void)
 {
@@ -91,6 +94,7 @@ static int __init pci_init(void)
      * Enable PCI passthrough when has been enabled explicitly
      * (pci-passthrough=on).
      */
+    int ret;
     if ( !is_pci_passthrough_enabled() && !arch_pci_device_physdevop() )
         return 0;
 
@@ -98,9 +102,26 @@ static int __init pci_init(void)
         panic("Could not initialize PCI segment 0\n");
 
     if ( acpi_disabled )
-        return dt_pci_init();
+        ret = dt_pci_init();
     else
-        return acpi_pci_init();
+        ret = acpi_pci_init();
+
+    if ( ret < 0 )
+        return ret;
+
+    if ( is_pci_scan_enabled() )
+    {
+        ret = scan_pci_devices();
+
+        if ( ret < 0 )
+            return ret;
+
+#ifdef CONFIG_PCI_DEVICE_DEBUG
+        dump_pci_devices('c');
+#endif
+    }
+
+    return 0;
 }
 __initcall(pci_init);
 
