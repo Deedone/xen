56a8b5fd25bc0a44e6c1c73917357a3b36a6a692
Luca Fancellu <luca.fancellu@arm.com>
2021-12-16 11:09:09 +0000

    xen/pci: helper function for hwdom using vpci
    When pci-scan is enabled and Xen supports vpci for guests, Xen will
    scan the pci bus to find devices and emulate the pci bus, so the hw
    domain must see the emulated bus instead of the real one.
    
    A new helper function, hwdom_uses_vpci, is implemented and returns
    true when pci-scan is enabled and Xen is built with
    CONFIG_HAS_VPCI_GUEST_SUPPORT=y.
    
    Issue-Id: SCM-1012
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Change-Id: Ifa6e0f91bde0cf524e91f811c1ac30afa3ed5213
    ---
    (cherry picked from commit 1b28ccb02aa12390f5941092e7f7671e2cae944d from
     the downstream branch poc/pci-passthrough from
     https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc.git)
    
    changes since cherry-pick:
    * s/always_inline/inline/
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>

--- xen/arch/x86/include/asm/pci.h
+++ xen/arch/x86/include/asm/pci.h
@@ -72,4 +72,7 @@ static always_inline bool arch_pci_device_physdevop(void)
     return false;
 }
 
+/* Unlike ARM, HW domain does not ever use vpci for x86 */
+static inline bool hwdom_uses_vpci(void)
+
 #endif /* __X86_PCI_H__ */
