844a479f1e1328f53ff1e37373f9d6fe8fe2b433
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2022-01-05 14:35:28 +0200

    [DOMD] xen: Implement PCI specific checks for hardware domain
    On x86 it is assumed that all the PCI host bridges live in a hardware
    domain and only Domain-0 is now supported. On ARM it is possible that
    real PCI host bridge hardware is passed through to a driver domain or
    other entity in the system, thus a simple is_hardware_domain check is
    not enough.
    
    Introduce a PCI dedicated pair of functions to check whether bridge's
    segment/bus physically owned by a domain given and means to get the owner
    domain of the bridge.
    
    Assume for now that all host bridges are owned by a single driver
    domain.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -35,6 +35,7 @@
 struct map_data {
     struct domain *d;
     const struct vpci_bar *bar;
+    const struct pci_dev *pdev;
     bool map;
 };
 
@@ -206,6 +207,7 @@ bool vpci_process_pending(struct vcpu *v)
             .d = v->domain,
             .map = v->vpci.cmd & PCI_COMMAND_MEMORY,
             .bar = bar,
+            .pdev = pdev,
         };
         int rc;
 
@@ -237,7 +239,7 @@ bool vpci_process_pending(struct vcpu *v)
 
             read_unlock(&v->domain->pci_lock);
 
-            if ( !is_hardware_domain(v->domain) )
+            if ( !pci_is_hardware_domain(v->domain, pdev->seg, pdev->bus) )
                 domain_crash(v->domain);
 
             return false;
@@ -266,7 +268,7 @@ static int __init apply_map(struct domain *d, const struct pci_dev *pdev,
     for ( i = 0; i < ARRAY_SIZE(header->bars); i++ )
     {
         struct vpci_bar *bar = &header->bars[i];
-        struct map_data data = { .d = d, .map = true, .bar = bar };
+        struct map_data data = { .d = d, .map = true, .bar = bar, .pdev = pdev };
 
         if ( rangeset_is_empty(bar->mem) )
             continue;
@@ -531,7 +533,7 @@ static void cf_check cmd_write(
 {
     struct vpci_header *header = data;
 
-    if ( !is_hardware_domain(pdev->domain) )
+    if ( !pci_is_hardware_domain(pdev->domain, pdev->seg, pdev->bus) )
     {
         const struct vpci *vpci = pdev->vpci;
         uint16_t excluded = PCI_COMMAND_PARITY | PCI_COMMAND_SERR |
@@ -586,7 +588,8 @@ static void cf_check bar_write(
     struct vpci_bar *bar = data;
     bool hi = false;
 
-    ASSERT(is_hardware_domain(pdev->domain));
+    ASSERT(pci_is_hardware_domain(pdev->domain, pdev->sbdf.seg,
+                                  pdev->sbdf.bus));
 
     if ( bar->type == VPCI_BAR_MEM64_HI )
     {
@@ -776,7 +779,7 @@ static int cf_check init_header(struct pci_dev *pdev)
     struct vpci_bar *bars = header->bars;
     int rc;
     bool mask_cap_list = false;
-    bool is_hwdom = is_hardware_pci_domain(pdev->domain);
+    bool is_hwdom = pci_is_hardware_domain(pdev->domain, pdev->seg, pdev->bus);
 
     ASSERT(rw_is_write_locked(&pdev->domain->pci_lock));
 
