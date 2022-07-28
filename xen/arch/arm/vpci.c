475fbf465fceafaae5556ba419b9ce15c51ad9ff
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2022-07-28 21:00:37 +0300

    xen/arm: translate virtual PCI bus topology for guests
    There are three  originators for the PCI configuration space access:
    1. The domain that owns physical host bridge: MMIO handlers are
    there so we can update vPCI register handlers with the values
    written by the hardware domain, e.g. physical view of the registers
    vs guest's view on the configuration space.
    2. Guest access to the passed through PCI devices: we need to properly
    map virtual bus topology to the physical one, e.g. pass the configuration
    space access to the corresponding physical devices.
    3. Emulated host PCI bridge access. It doesn't exist in the physical
    topology, e.g. it can't be mapped to some physical host bridge.
    So, all access to the host bridge itself needs to be trapped and
    emulated.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    ---
    In v11:
    - Fixed format issues
    - Added ASSERT_UNREACHABLE() to the dummy implementation of
    vpci_translate_virtual_device()
    - Moved variable in vpci_sbdf_from_gpa(), now it is easier to follow
    the logic in the function
    Since v9:
    - Commend about required lock replaced with ASSERT()
    - Style fixes
    - call to vpci_translate_virtual_device folded into vpci_sbdf_from_gpa
    Since v8:
    - locks moved out of vpci_translate_virtual_device()
    Since v6:
    - add pcidevs locking to vpci_translate_virtual_device
    - update wrt to the new locking scheme
    Since v5:
    - add vpci_translate_virtual_device for #ifndef CONFIG_HAS_VPCI_GUEST_SUPPORT
      case to simplify ifdefery
    - add ASSERT(!is_hardware_domain(d)); to vpci_translate_virtual_device
    - reset output register on failed virtual SBDF translation
    Since v4:
    - indentation fixes
    - constify struct domain
    - updated commit message
    - updates to the new locking scheme (pdev->vpci_lock)
    Since v3:
    - revisit locking
    - move code to vpci.c
    Since v2:
     - pass struct domain instead of struct vcpu
     - constify arguments where possible
     - gate relevant code with CONFIG_HAS_VPCI_GUEST_SUPPORT
    New in v2

--- xen/arch/arm/vpci.c
+++ xen/arch/arm/vpci.c
@@ -7,23 +7,35 @@
 
 #include <asm/mmio.h>
 
-static pci_sbdf_t vpci_sbdf_from_gpa(const struct pci_host_bridge *bridge,
-                                     paddr_t gpa, bool use_root)
+static bool vpci_sbdf_from_gpa(struct domain *d,
+                               const struct pci_host_bridge *bridge,
+                               paddr_t gpa, bool use_root, pci_sbdf_t *sbdf)
 {
-    pci_sbdf_t sbdf;
+    bool translated = true;
+
+    ASSERT(sbdf);
 
     if ( bridge )
     {
         const struct pci_config_window *cfg = use_root ? bridge->cfg :
                                                          bridge->child_cfg;
-        sbdf.sbdf = VPCI_ECAM_BDF(gpa - cfg->phys_addr);
-        sbdf.seg = bridge->segment;
-        sbdf.bus += cfg->busn_start;
+        sbdf->sbdf = VPCI_ECAM_BDF(gpa - cfg->phys_addr);
+        sbdf->seg = bridge->segment;
+        sbdf->bus += cfg->busn_start;
     }
     else
-        sbdf.sbdf = VPCI_ECAM_BDF(gpa - GUEST_VPCI_ECAM_BASE);
+    {
+        /*
+         * For the passed through devices we need to map their virtual SBDF
+         * to the physical PCI device being passed through.
+         */
+        sbdf->sbdf = VPCI_ECAM_BDF(gpa - GUEST_VPCI_ECAM_BASE);
+        read_lock(&d->pci_lock);
+        translated = vpci_translate_virtual_device(d, sbdf);
+        read_unlock(&d->pci_lock);
+    }
 
-    return sbdf;
+    return translated;
 }
 
 static int vpci_mmio_read(struct vcpu *v, mmio_info_t *info, register_t *r,
@@ -50,7 +62,17 @@ static int vpci_mmio_read_root(struct vcpu *v, mmio_info_t *info,
                           register_t *r, void *p)
 {
     struct pci_host_bridge *bridge = p;
-    pci_sbdf_t sbdf = vpci_sbdf_from_gpa(bridge, info->gpa, true);
+    pci_sbdf_t sbdf;
+    const uint8_t access_size = (1 << info->dabt.size) * 8;
+    const uint64_t access_mask = GENMASK_ULL(access_size - 1, 0);
+
+    ASSERT(!bridge == !is_hardware_domain(v->domain));
+
+    if ( !vpci_sbdf_from_gpa(v->domain, bridge, info->gpa, true, &sbdf) )
+    {
+        *r = access_mask;
+        return 1;
+    }
 
     return vpci_mmio_read(v, info, r, sbdf);
 }
@@ -59,7 +81,17 @@ static int vpci_mmio_read_child(struct vcpu *v, mmio_info_t *info,
                           register_t *r, void *p)
 {
     struct pci_host_bridge *bridge = p;
-    pci_sbdf_t sbdf = vpci_sbdf_from_gpa(bridge, info->gpa, false);
+    pci_sbdf_t sbdf;
+    const uint8_t access_size = (1 << info->dabt.size) * 8;
+    const uint64_t access_mask = GENMASK_ULL(access_size - 1, 0);
+
+    ASSERT(!bridge == !is_hardware_domain(v->domain));
+
+    if ( !vpci_sbdf_from_gpa(v->domain, bridge, info->gpa, true, &sbdf) )
+    {
+        *r = access_mask;
+        return 1;
+    }
 
     return vpci_mmio_read(v, info, r, sbdf);
 }
