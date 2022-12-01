7611300335a1bee29e807c04f8ead2685cf3a0c6
Rahul Singh <rahul.singh@arm.com>
2022-12-01 16:02:45 +0000

    xen/arm: vIOMMU: Modify the partial device tree for dom0less
    To configure IOMMU in guest for passthrough devices, user will need to
    copy the unmodified "iommus" property from host device tree to partial
    device tree. To enable the dom0 linux kernel to confiure the IOMMU
    correctly replace the phandle in partial device tree with virtual
    IOMMU phandle when "iommus" property is set.
    
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>
    Signed-off-by: Milan Djokic <milan_djokic@epam.com>

--- xen/common/device-tree/dom0less-build.c
+++ xen/common/device-tree/dom0less-build.c
@@ -446,7 +446,42 @@ static int __init handle_prop_pfdt(struct kernel_info *kinfo,
     return ( propoff != -FDT_ERR_NOTFOUND ) ? propoff : 0;
 }
 
-static int __init scan_pfdt_node(struct kernel_info *kinfo, const void *pfdt,
+#ifdef CONFIG_ARM_VIRTUAL_IOMMU
+static void __init modify_pfdt_node(void *pfdt, int nodeoff, struct domain *d)
+{
+    int proplen, i, rc;
+    const fdt32_t *prop;
+    fdt32_t *prop_c;
+
+    prop = fdt_getprop(pfdt, nodeoff, "iommus", &proplen);
+    if ( !prop )
+        return;
+
+    prop_c = xzalloc_bytes(proplen);
+    if ( !prop_c )
+        return;
+
+    for ( i = 0; i < proplen / 8; ++i )
+    {
+        prop_c[i * 2] = cpu_to_fdt32(GUEST_PHANDLE_VSMMUV3);
+        prop_c[i * 2 + 1] = prop[i * 2 + 1];
+    }
+
+    rc = fdt_setprop(pfdt, nodeoff, "iommus", prop_c, proplen);
+    if ( rc )
+    {
+        dprintk(XENLOG_ERR, "Can't set the iommus property in partial FDT");
+        return;
+    }
+
+    return;
+}
+#else
+    static void __init modify_pfdt_node(void *pfdt, int nodeoff,
+                                        struct domain *d) {}
+#endif
+
+static int __init scan_pfdt_node(struct kernel_info *kinfo, void *pfdt,
                                  int nodeoff,
                                  uint32_t address_cells, uint32_t size_cells,
                                  bool scan_passthrough_prop)
@@ -472,6 +507,7 @@ static int __init scan_pfdt_node(struct kernel_info *kinfo, const void *pfdt,
     node_next = fdt_first_subnode(pfdt, nodeoff);
     while ( node_next > 0 )
     {
+        modify_pfdt_node(pfdt, node_next);
         rc = scan_pfdt_node(kinfo, pfdt, node_next, address_cells, size_cells,
                             scan_passthrough_prop);
         if ( rc )
