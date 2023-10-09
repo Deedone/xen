02709c7975b74bf54d81a88c204e1aeb8fa74b30
Stewart Hildebrand <stewart.hildebrand@amd.com>
2023-10-09 10:17:59 -0400

    [FUTURE] xen/arm: enable vPCI for dom0
    Set the vPCI flag in xen_domctl_createdomain to enable vPCI for dom0 if
    iommu and PCI passthrough are enabled and there exists a PCI host bridge
    in the system.
    
    Adjust pci_host_iterate_bridges_and_count() to count the number of host
    bridges if no callback is provided.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    Allowing/enabling vPCI for dom0 on ARM should follow or be part of the
    PCI passthrough SMMU series [1]. I'm including it here due to
    prerequisites in this Kconfig series. Once the prerequisites are
    committed I'll move this patch to the PCI passthrough SMMU series.
    
    v5->v6:
    * no change
    
    v4->v5:
    * add [FUTURE] tag
    * move flags_optional change from the previous patch to here
    
    v3->v4:
    * depend on iommu_enabled, pci_passthrough_enabled, and whether there
      is a pci host bridge
    
    v2->v3:
    * new patch
    
    [1] https://lists.xenproject.org/archives/html/xen-devel/2023-10/msg00210.html

--- xen/arch/arm/include/asm/pci.h
+++ xen/arch/arm/include/asm/pci.h
@@ -183,6 +183,15 @@ static inline int pci_get_new_domain_nr(void)
     return -1;
 }
 
+struct pci_host_bridge;
+
+static inline int pci_host_iterate_bridges_and_count(
+    struct domain *d,
+    int (*cb)(struct domain *d, struct pci_host_bridge *bridge))
+{
+    return 0;
+}
+
 #endif  /*!CONFIG_HAS_PCI*/
 
 #ifdef CONFIG_HAS_VPCI_GUEST_SUPPORT
