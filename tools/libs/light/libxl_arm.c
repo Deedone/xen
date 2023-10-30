865deee82252aca5b51b66129a6d63f67cef081f
Stewart Hildebrand <stewart.hildebrand@amd.com>
2023-10-30 12:42:45 -0400

    [FUTURE] tools/arm: enable vPCI for domUs
    Set the vPCI flag in xen_domctl_createdomain to enable vPCI if a pci
    device has been specified in the xl domain config file.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    Same story as the patch before this regarding the [FUTURE] tag.
    
    v5->v6:
    * no change
    
    v4->v5:
    * no change
    
    v3->v4:
    * split from ("xen/arm: enable vPCI for domUs")

--- tools/libs/light/libxl_arm.c
+++ tools/libs/light/libxl_arm.c
@@ -233,6 +233,9 @@ int libxl__arch_domain_prepare_config(libxl__gc *gc,
         config->arch.sve_vl = d_config->b_info.arch_arm.sve_vl / 128U;
     }
 
+    if (d_config->num_pcidevs)
+        config->flags |= XEN_DOMCTL_CDF_vpci;
+
     switch (d_config->b_info.arch_arm.arm_sci.type) {
     case LIBXL_ARM_SCI_TYPE_NONE:
         config->arch.arm_sci_type = XEN_DOMCTL_CONFIG_ARM_SCI_NONE;
