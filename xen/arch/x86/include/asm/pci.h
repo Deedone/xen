3362d0aa10870c8022ccf22aa4e8a8d887e0eab6
Stewart Hildebrand <stewart.hildebrand@amd.com>
2024-01-11 13:11:33 -0500

    xen/pci: helpers to get/reserve BAR ranges
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>

--- xen/arch/x86/include/asm/pci.h
+++ xen/arch/x86/include/asm/pci.h
@@ -74,6 +74,20 @@ bool pci_check_bar(const struct pci_dev *pdev, mfn_t start, mfn_t end);
 struct rangeset;
 int pci_sanitize_bar_memory(struct rangeset *r);
 
+static inline uint64_t pci_get_new_bar_addr(const struct pci_dev *pdev,
+                                            uint64_t size, bool is_64bit,
+                                            bool prefetch)
+{
+    return 0;
+}
+
+static inline int pci_reserve_bar_range(const struct pci_dev *pdev,
+                                        uint64_t addr, uint64_t size,
+                                        bool prefetch)
+{
+    return 0;
+}
+
 /* Unlike ARM, HW domain does not ever use vpci for x86 */
 static inline bool hwdom_uses_vpci(void)
 {
