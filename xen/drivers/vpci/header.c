1b74d2c6f02e1852e95af492d741dae9fa7033de
Stewart Hildebrand <stewart.hildebrand@amd.com>
2024-02-05 15:00:40 -0500

    [RFC] vpci: allow BAR write while mapped
    Xen vPCI refuses BAR writes if the BAR is mapped in p2m. If firmware
    initialized the BAR to a bad address, Linux will try to write a new
    address to the BAR without disabling memory decoding. Allow the write
    by updating p2m right away in the vPCI BAR write handler.
    
    Resolves: https://gitlab.com/xen-project/xen/-/issues/197
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    RFC: Currently the deferred mapping machinery supports only map or
         unmap, not both. It might be better to rework the mapping machinery
         to support unmap-then-map operations, but please let me know your
         thoughts.
    RFC: This patch has not yet made an attempt to distinguish between
         32-bit and 64-bit writes. It probably should.
    
    RFC->v1:
    * remove gprintk
    * TODO: keep memory decoding enabled
    * TODO: 32-bit BARs only
    * TODO: add unmap-then-map operation to deferred mapping machinery

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -173,7 +173,7 @@ static void modify_decoding(const struct pci_dev *pdev, uint16_t cmd,
         ASSERT_UNREACHABLE();
 }
 
-bool vpci_process_pending(struct vcpu *v)
+static bool process_pending(struct vcpu *v, bool need_lock)
 {
     switch ( v->vpci.task )
     {
@@ -186,12 +186,14 @@ bool vpci_process_pending(struct vcpu *v)
         if ( !pdev )
             break;
 
-        read_lock(&v->domain->pci_lock);
+        if ( need_lock )
+            read_lock(&v->domain->pci_lock);
 
         if ( !pdev->vpci || (v->domain != pdev->domain) )
         {
             v->vpci.memory.pdev = NULL;
-            read_unlock(&v->domain->pci_lock);
+            if ( need_lock )
+                read_unlock(&v->domain->pci_lock);
             break;
         }
 
@@ -214,7 +216,8 @@ bool vpci_process_pending(struct vcpu *v)
 
             if ( rc == -ERESTART )
             {
-                read_unlock(&v->domain->pci_lock);
+                if ( need_lock )
+                    read_unlock(&v->domain->pci_lock);
                 return true;
             }
 
@@ -233,7 +236,8 @@ bool vpci_process_pending(struct vcpu *v)
 
                 v->vpci.memory.pdev = NULL;
 
-                read_unlock(&v->domain->pci_lock);
+                if ( need_lock )
+                    read_unlock(&v->domain->pci_lock);
 
                 if ( !is_hardware_domain(v->domain) )
                     domain_crash(v->domain);
@@ -243,11 +247,14 @@ bool vpci_process_pending(struct vcpu *v)
         }
         v->vpci.memory.pdev = NULL;
 
-        spin_lock(&pdev->vpci->lock);
+        if ( need_lock )
+            spin_lock(&pdev->vpci->lock);
         modify_decoding(pdev, v->vpci.memory.cmd, v->vpci.memory.rom_only);
-        spin_unlock(&pdev->vpci->lock);
+        if ( need_lock )
+            spin_unlock(&pdev->vpci->lock);
 
-        read_unlock(&v->domain->pci_lock);
+        if ( need_lock )
+            read_unlock(&v->domain->pci_lock);
 
         break;
     }
@@ -262,6 +269,11 @@ bool vpci_process_pending(struct vcpu *v)
 
     v->vpci.task = NONE;
     return false;
+}
+
+bool vpci_process_pending(struct vcpu *v)
+{
+    return process_pending(v, true);
 }
 
 static int apply_map(struct domain *d, const struct pci_dev *pdev,
@@ -585,6 +597,8 @@ static void cf_check bar_write(
 {
     struct vpci_bar *bar = data;
     bool hi = false;
+    bool reenable = false;
+    uint16_t cmd = 0;
 
     ASSERT(is_hardware_domain(pdev->domain));
 
@@ -605,10 +619,28 @@ static void cf_check bar_write(
     {
         /* If the value written is the current one avoid printing a warning. */
         if ( val != (uint32_t)(bar->addr >> (hi ? 32 : 0)) )
-            gprintk(XENLOG_WARNING,
-                    "%pp: ignored BAR %zu write while mapped\n",
-                    &pdev->sbdf, bar - pdev->vpci->header.bars + hi);
-        return;
+        {
+            ASSERT(rw_is_write_locked(&pdev->domain->pci_lock));
+            ASSERT(spin_is_locked(&pdev->vpci->lock));
+            reenable = true;
+            cmd = pci_conf_read16(pdev->sbdf, PCI_COMMAND);
+            /*
+             * Write-while-mapped: unmap the old BAR in p2m. We want this to
+             * finish right away since the deferral machinery only supports
+             * unmap OR map, not unmap-then-remap. Ultimately, it probably would
+             * be better to defer the write-while-mapped case just like regular
+             * BAR writes (but still only allow it for 32-bit BAR writes).
+             */
+            /* TODO: don't disable memory decoding */
+            vpci_modify_bars(pdev, cmd & ~PCI_COMMAND_MEMORY, false, false);
+            /* Call process pending here to ensure P2M operations are done */
+            while ( process_pending(current, false) )
+            {
+                /* Pre-empted, try again */
+            }
+        }
+        else
+            return;
     }
 
 
@@ -630,6 +662,10 @@ static void cf_check bar_write(
     }
 
     pci_conf_write32(pdev->sbdf, reg, val);
+
+    if ( reenable )
+        /* Write-while-mapped: map the new BAR in p2m. OK to defer. */
+        vpci_modify_bars(pdev, cmd, false, false);
 }
 
 void cf_check vpci_guest_mem_bar_write(const struct pci_dev *pdev,
