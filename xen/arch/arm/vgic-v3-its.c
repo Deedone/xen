0b68199fdaac395e9437781350efc3776c3a3779
Stewart Hildebrand <stewart.hildebrand@amd.com>
2024-02-19 12:19:08 -0500

    xen/arm: map ITS doorbell registers for domUs
    Map a guest ITS doorbell for domUs at GUEST_GICV3_ITS_BASE +
    ITS_DOORBELL_OFFSET. Map it to first real ITS doorbell found in our list
    of hardware ITSes, so at the moment only hardware with a single ITS is
    supported.
    
    Remove BUG_ON() from arm_iommu_map_page() to allow translated iommu
    mappings.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/vgic-v3-its.c
+++ xen/arch/arm/vgic-v3-its.c
@@ -1551,22 +1551,56 @@ static int vgic_v3_its_init_virtual(struct domain *d, paddr_t guest_addr,
 
     if ( is_iommu_enabled(its->d) )
     {
-        mfn_t mfn = maddr_to_mfn(host_doorbell_addr);
-        unsigned int flush_flags = 0;
-        int ret = iommu_map(its->d, _dfn(PFN_DOWN(its->doorbell_address)),
+        mfn_t mfn;
+        unsigned int flush_flags;
+        int ret;
+
+
+        if ( is_hardware_domain(d) )
+        {
+            mfn = maddr_to_mfn(host_doorbell_addr);
+            flush_flags = 0;
+            ret = iommu_map(its->d, _dfn(PFN_DOWN(its->doorbell_address)),
                             mfn, 1, IOMMUF_writable, &flush_flags);
 
-        if ( ret < 0 )
-        {
-            printk(XENLOG_G_ERR
-                    "GICv3: Map ITS translation register for %pd failed.\n",
-                    its->d);
-            return ret;
-        }
+            if ( ret < 0 )
+            {
+                printk(XENLOG_G_ERR
+                        "GICv3: Map ITS translation register for %pd failed.\n",
+                        its->d);
+                return ret;
+            }
 
-        ret = iommu_iotlb_flush(its->d, _dfn(PFN_DOWN(its->doorbell_address)), 1, flush_flags);
-        if ( ret < 0 )
-            return ret;
+            ret = iommu_iotlb_flush(its->d,
+                                    _dfn(PFN_DOWN(its->doorbell_address)),
+                                    1, flush_flags);
+            if ( ret < 0 )
+                return ret;
+        }
+        else
+        {
+            struct host_its *hw_its;
+            dfn_t dfn = _dfn(mfn_x(maddr_to_mfn(its->doorbell_address)));
+
+            list_for_each_entry(hw_its, &host_its_list, entry)
+            {
+                mfn = maddr_to_mfn(hw_its->addr + ITS_DOORBELL_OFFSET);
+                flush_flags = 0;
+                ret = iommu_map(its->d, dfn, mfn, 1, IOMMUF_writable,
+                                &flush_flags);
+
+                if ( ret < 0 )
+                {
+                    gprintk(XENLOG_ERR,
+                            "GICv3: Map ITS translation register %pd failed.\n",
+                            its->d);
+                    return ret;
+                }
+
+                /* XXX: At the moment we only support a single hardware ITS */
+                break;
+            }
+        }
     }
 
     /* Register the virtual ITS to be able to clean it up later. */
