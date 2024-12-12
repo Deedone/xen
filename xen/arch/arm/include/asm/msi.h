653864e16102b21bad8a17931d8259e9e206826c
Mykyta Poturai <mykyta_poturai@epam.com>
2024-12-12 11:55:34 +0200

    arm: msi: Make ARM compilable with CONFIG_HAS_PCI_MSI
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>

---
+++ xen/arch/arm/include/asm/msi.h
@@ -0,0 +1,16 @@
+
+#ifndef __ARM_MSI_H
+#define __ARM_MSI_H
+
+
+/* Arch-specific MSI data for vPCI. */
+struct vpci_arch_msi {
+};
+
+/* Arch-specific MSI-X entry data for vPCI. */
+struct vpci_arch_msix_entry {
+};
+
+
+
+#endif
