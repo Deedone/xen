9ef1efc1013886cb69f520a7944ae762f6b0ee06
Mykyta Poturai <mykyta_poturai@epam.com>
2024-12-12 11:57:21 +0200

    flask: msi: Make flask MSI checks architecture independent
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>

--- xen/include/xen/msi.h
+++ xen/include/xen/msi.h
@@ -40,6 +40,7 @@ int pdev_msi_init(struct pci_dev *pdev);
 void pdev_msi_deinit(struct pci_dev *pdev);
 void pdev_dump_msi(const struct pci_dev *pdev);
 extern void pci_cleanup_msi(struct pci_dev *pdev);
+pci_sbdf_t msi_irq_to_sbdf(int irq);
 
 #else /* !CONFIG_HAS_PCI_MSI */
 
