9d34330ff49d48ba61335ed27eebee3894ab3de4
Stewart Hildebrand <stewart.hildebrand@amd.com>
2025-01-31 08:52:27 -0500

    xen/vpci: arm: keep MSI-X p2m mappings on Arm
    There's no need to intercept the MSI-X tables on Arm. Keep the p2m
    mappings for the MSI-X and PBA tables, and remove the mmio handler.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -420,6 +420,8 @@ int vpci_modify_bars(const struct pci_dev *pdev, uint16_t cmd, bool rom_only)
     }
 
     /* Remove any MSIX regions if present. */
+    if ( IS_ENABLED(CONFIG_X86) )
+    {
     for ( i = 0; msix && i < ARRAY_SIZE(msix->tables); i++ )
     {
         unsigned long start = PFN_DOWN(vmsix_table_addr(pdev->vpci, i));
@@ -442,6 +444,7 @@ int vpci_modify_bars(const struct pci_dev *pdev, uint16_t cmd, bool rom_only)
                 return rc;
             }
         }
+    }
     }
 
     /*
