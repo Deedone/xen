37523c6ff5576a3f55341a3d5fbe113ea9ff801d
Stewart Hildebrand <stewart.hildebrand@amd.com>
2025-03-19 10:10:33 -0400

    vpci: rename and export vpci_bar_add_rangeset
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -780,8 +780,8 @@ static void cf_check rom_write(
     }
 }
 
-static int bar_add_rangeset(const struct pci_dev *pdev, struct vpci_bar *bar,
-                            unsigned int i)
+int vpci_bar_add_rangeset(const struct pci_dev *pdev, struct vpci_bar *bar,
+                          unsigned int i)
 {
     char str[32];
 
@@ -996,7 +996,7 @@ static int cf_check init_header(struct pci_dev *pdev)
         else
             bars[i].type = VPCI_BAR_MEM32;
 
-        rc = bar_add_rangeset(pdev, &bars[i], i);
+        rc = vpci_bar_add_rangeset(pdev, &bars[i], i);
         if ( rc )
             goto fail;
 
@@ -1054,7 +1054,7 @@ static int cf_check init_header(struct pci_dev *pdev)
             rom->type = VPCI_BAR_EMPTY;
         else
         {
-            rc = bar_add_rangeset(pdev, rom, num_bars);
+            rc = vpci_bar_add_rangeset(pdev, rom, num_bars);
             if ( rc )
                 goto fail;
         }
