f4d48063a4091cf851eab7ad9bf53364df8d1568
Stewart Hildebrand <stewart.hildebrand@amd.com>
2025-03-19 10:56:00 -0400

    vpci: rename and export vpci_modify_bars
    Export functions required for SR-IOV support.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Reviewed-by: Teddy Astie <teddy.astie@vates.tech>
    
    ---
    v1->v2
    * Collect RBs

--- xen/include/xen/vpci.h
+++ xen/include/xen/vpci.h
@@ -295,6 +295,9 @@ bool vpci_ecam_write(pci_sbdf_t sbdf, unsigned int reg, unsigned int len,
 bool vpci_ecam_read(pci_sbdf_t sbdf, unsigned int reg, unsigned int len,
                     unsigned long *data);
 
+/* Map/unmap the BARs of a vPCI device. */
+int vpci_modify_bars(const struct pci_dev *pdev, uint16_t cmd, bool rom_only);
+
 #endif /* __XEN__ */
 
 #else /* !CONFIG_HAS_VPCI */
