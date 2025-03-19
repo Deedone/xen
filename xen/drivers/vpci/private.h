20aeeab97c08890bbad1f557d55d310030de3958
Stewart Hildebrand <stewart.hildebrand@amd.com>
2025-03-19 15:22:19 -0400

    vpci: rename and export vpci_guest_mem_bar_{read,write}
    Export functions required for SR-IOV support.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Reviewed-by: Teddy Astie <teddy.astie@vates.tech>
    ---
    v2->v3:
    * move declarations to private.h
    v1->v2:
    * collect RBs

--- xen/drivers/vpci/private.h
+++ xen/drivers/vpci/private.h
@@ -141,6 +141,12 @@ static inline size_t vmsix_table_size(const struct vpci *vpci, unsigned int nr)
 /* Map/unmap the BARs of a vPCI device. */
 int vpci_modify_bars(const struct pci_dev *pdev, uint16_t cmd, bool rom_only);
 
+void cf_check vpci_guest_mem_bar_write(const struct pci_dev *pdev,
+                                       unsigned int reg, uint32_t val,
+                                       void *data);
+
+uint32_t cf_check vpci_guest_mem_bar_read(const struct pci_dev *pdev,
+                                          unsigned int reg, void *data);
 #endif /* __XEN__ */
 
 #endif /* VPCI_PRIVATE_H */
