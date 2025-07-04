015933fc565c6e4f6f9a674a9fe3c9e99205b62a
Alejandro Vallejo <alejandro.garciavallejo@amd.com>
2025-07-04 18:16:36 +0200

    dom0less: Parse memory properties in the common bindings
    Move the "memory" binding from dom0less-build to dom0less-bindings.
    
    Memory specified in the device tree overrides the dom0_mem parameters.
    
    Signed-off-by: Alejandro Vallejo <alejandro.garciavallejo@amd.com>
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>
    Reviewed-by: Jason Andryuk <jason.andryuk@amd.com>

--- xen/common/device-tree/dom0less-bindings.c
+++ xen/common/device-tree/dom0less-bindings.c
@@ -167,5 +167,8 @@ int __init parse_dom0less_node(struct dt_device_node *node,
         bd->domid = val;
     }
 
+    if ( !dt_property_read_u64(node, "memory", &bd->memory) )
+        panic("missing memory binding for %s.\n", dt_node_name(node));
+
     return arch_parse_dom0less_node(node, bd);
 }
