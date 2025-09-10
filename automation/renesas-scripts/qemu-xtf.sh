716129775c3f60c13142960f78b0f7352875263d
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2025-09-10 09:03:43 +0000

    CI: run qemu-xtf test with domU
    Run test with one additional Linux guest domain.
    
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>

--- automation/renesas-scripts/qemu-xtf.sh
+++ automation/renesas-scripts/qemu-xtf.sh
@@ -15,7 +15,34 @@ shift
 
 set -x
 
+
 export XEN_ROOT="${PWD}"
+
+# DomU Busybox
+cd binaries
+mkdir -p initrd
+mkdir -p initrd/bin
+mkdir -p initrd/sbin
+mkdir -p initrd/etc
+mkdir -p initrd/dev
+mkdir -p initrd/proc
+mkdir -p initrd/sys
+mkdir -p initrd/lib
+mkdir -p initrd/var
+mkdir -p initrd/mnt
+cp /bin/busybox initrd/bin/busybox
+initrd/bin/busybox --install initrd/bin
+echo "#!/bin/sh
+
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev
+/bin/sh" > initrd/init
+chmod +x initrd/init
+cd initrd
+find . | cpio -H newc -o | gzip > ../domU-rootfs.cpio.gz
+cd .. # binaries
+cd .. # XEN_ROOT
 cd $(dirname $0)
 
 source include/xtf-runner
