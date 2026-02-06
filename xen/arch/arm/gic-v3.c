7dc8197f9041e3da9d2551910e30e3018332d74a
Ayan Kumar Halder <ayan.kumar.halder@amd.com>
2026-02-06 21:25:35 +0100

    xen/arm: gic-v3: Diable gicv3_iomem_deny_access
    This is invoked only from acpi/*
    
    Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
    Reviewed-by: Michal Orzel <michal.orzel@amd.com>

--- xen/arch/arm/gic-v3.c
+++ xen/arch/arm/gic-v3.c
@@ -1610,6 +1610,7 @@ static void __init gicv3_dt_init(void)
                               &vbase, &vsize);
 }
 
+
 static int gicv3_iomem_deny_access(struct domain *d)
 {
     int rc, i;
@@ -2509,7 +2510,8 @@ static const struct gic_hw_operations gicv3_ops = {
     .make_hwdom_madt     = gicv3_make_hwdom_madt,
     .get_hwdom_extra_madt_size = gicv3_get_hwdom_extra_madt_size,
 #endif
-    .iomem_deny_access   = gicv3_iomem_deny_access,
+    .iomem_deny_access   = IS_ENABLED(CONFIG_ACPI) ?
+                           gicv3_iomem_deny_access : NULL,
     .do_LPI              = gicv3_do_LPI,
 #ifdef CONFIG_SYSTEM_SUSPEND
     .suspend             = gicv3_suspend,
