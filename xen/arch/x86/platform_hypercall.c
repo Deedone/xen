d1d7c9e7b378b061f89c65d02abf613566566711
Mykyta Poturai <mykyta_poturai@epam.com>
2026-03-30 10:57:23 +0300

    Kconfig: Make cpu hotplug configurable
    For the purposes of certification, we want as little code as possible to
    be unconditionally compiled in. Make CPU hotplug and SMT operations
    configurable to ease the process. This will also help with introducing
    CPU hotplug on Arm, where it needs to be configurable.
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    ---
    v7->v8:
    * fix style
    * s/CPU_HOTPLUG/CPU_ONLINE_OFFLINE/
    
    v6->v7:
    * new patch

--- xen/arch/x86/platform_hypercall.c
+++ xen/arch/x86/platform_hypercall.c
@@ -735,6 +735,16 @@ ret_t do_platform_op(
     {
         int cpu = op->u.cpu_ol.cpuid;
 
+        if ( !IS_ENABLED(CONFIG_CPU_ONLINE_OFFLINE) )
+        {
+            ret = -EOPNOTSUPP;
+            break;
+        }
+
+        ret = xsm_resource_plug_core(XSM_HOOK);
+        if ( ret )
+            break;
+
         if ( cpu >= nr_cpu_ids || !cpu_present(cpu) ||
              clocksource_is_tsc() )
         {
@@ -756,6 +766,16 @@ ret_t do_platform_op(
     case XENPF_cpu_offline:
     {
         int cpu = op->u.cpu_ol.cpuid;
+
+        if ( !IS_ENABLED(CONFIG_CPU_ONLINE_OFFLINE) )
+        {
+            ret = -EOPNOTSUPP;
+            break;
+        }
+
+        ret = xsm_resource_unplug_core(XSM_HOOK);
+        if ( ret )
+            break;
 
         if ( cpu == 0 )
         {
