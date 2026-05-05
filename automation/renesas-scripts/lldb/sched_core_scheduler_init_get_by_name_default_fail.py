c4767d527552e2056beddc758e96d833ac74a3e9
Ruslan Ruslichenko <Ruslan_Ruslichenko@epam.com>
2026-05-05 11:08:22 +0200

    ci: lldb: fix sched_core_scheduler_init_get_by_name_default_fail test
    As sched_get_by_name() is inline function, breakpoint on it may fail.
    Change test logic to corrupt the name of default scheduler, which
    should cause sched_get_by_name() to fail as expected by test.
    The test assumes that default scheduler is 'credit2'.
    
    Signed-off-by: Ruslan Ruslichenko <Ruslan_Ruslichenko@epam.com>

--- automation/renesas-scripts/lldb/sched_core_scheduler_init_get_by_name_default_fail.py
+++ automation/renesas-scripts/lldb/sched_core_scheduler_init_get_by_name_default_fail.py
@@ -22,17 +22,41 @@ def Panic(frame):
     sys.stdout.flush()
     os._exit(0)
 
-def SchedulerGetByName(frame):
+def SchedulerInit(frame):
     start_addr = dbg.evaluate_expression_int(frame, "&__start_schedulers_array")
     end_addr = dbg.evaluate_expression_int(frame, "&__end_schedulers_array")
 
     num_schedulers = (end_addr - start_addr) // 8
     print(f"[+] Found {num_schedulers} schedulers")
 
-    # Ensure sched_get_by_name() returns 'NULL' by corrupting all schedulers
+    # Clear opt_sched cmdline parameter
+    opt_sched_addr = dbg.evaluate_expression_int(frame, "&opt_sched[0]")
+
+    dbg.evaluate_expression(frame, f"*(char*){opt_sched_addr}=0x0")
+
+    # Ensure sched_get_by_name() returns 'NULL' by corrupting default scheduler
+    # Assume CONFIG_SCHED_DEFAULT="credit2"
     for i in range(num_schedulers):
-        expr = f"((struct scheduler **){start_addr})[{i}] = 0"
-        dbg.evaluate_expression(frame, expr)
+        name_addr_expr = f"((struct scheduler **){start_addr})[{i}]->opt_name"
+        name_addr = dbg.evaluate_expression_int(frame, name_addr_expr)
+
+        name = dbg.evaluate_expression_str(frame, f"(char*){name_addr}")
+
+        if name == "credit2":
+            print(f"[+] Found 'credit2' scheduler at index '{i}'")
+
+            # Corrupt 'credit2' scheduler name to cause sched_get_by_name() fail
+            name_addr += 1
+            dbg.evaluate_expression(frame,
+                        f"((struct scheduler **){start_addr})[{i}]->opt_name = (char*){name_addr}")
+
+            name_addr = dbg.evaluate_expression_int(frame, name_addr_expr)
+
+            name = dbg.evaluate_expression_str(frame, f"(char*){name_addr}")
+
+            print(f"[+] Updated name to: {name}")
+
+            break
 
 
 elf_path = os.environ.get("XEN_ELF", "xen")
@@ -40,7 +64,7 @@ port = int(os.environ.get("XEN_PORT", "1234"))
 
 try:
     dbg.connect(elf_path, port)
-    dbg.install_entry_hook("sched_get_by_name", SchedulerGetByName)
+    dbg.install_entry_hook("scheduler_init", SchedulerInit)
     dbg.install_entry_hook("panic", Panic)
     dbg.resume()
 
