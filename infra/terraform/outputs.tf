0486828714648b997d0493ea24c85e18a434a517
Vasyl Samoilov <vasyl.samoilov@gmail.com>
2026-05-14 16:49:28 +0300

    ci: disable x86 runner (not needed for Renesas pipeline)

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -7,8 +7,3 @@ output "cache_bucket_arm64" {
   description = "S3 cache bucket for arm64 runners"
   value       = module.gitlab_runner_arm64.runner_cache_bucket_name
 }
-
-output "cache_bucket_x86" {
-  description = "S3 cache bucket for x86 runners"
-  value       = module.gitlab_runner_x86.runner_cache_bucket_name
-}
