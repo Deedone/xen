24d2e202429d61fbef4d12acd8b79e4362cc0334
Dmytro Prokopchuk <dmytro_prokopchuk1@epam.com>
2026-05-18 19:05:56 +0300

    libafl-qemu: mark exit helpers as noinline
    The LibAFL sync_exit helpers use inline assembly to emit a custom
    instruction opcode consumed by the QEMU backend.
    
    The GCC can perform aggressive optimization and may inline these
    helpers into callers. In the inlined form, register allocation/
    reordering around the inline asm can corrupt the expected
    AArch64 calling convention state used by the QEMU trap handler.
    
    This resulted in failures such as:
    
        thread 'main' (71) panicked at src/fuzzer.rs:222:36:
        called `Result::unwrap()` on an `Err` value: QemuExitReasonError(CommandError(UnknownCommand(5126048)))
    
    The broken code generation emitted the opcode directly inside callers:
    
     2f0:	d2800080 	mov	x0, #0x4                   	// #4
     2f4:	66f23a0f 	.word	0x66f23a0f
    
    while the working variant emits a proper function call boundary:
    
     2f8:	d2800080 	mov	x0, #0x4                   	// #4
     2fc:	94000000 	bl	34 <_libafl_sync_exit_call1>
    
    Mark the helpers as noinline to prevent the compiler from inlining the
    asm wrappers into callers.
    
    Signed-off-by: Dmytro Prokopchuk <dmytro_prokopchuk1@epam.com>

--- xen/arch/arm/include/asm/libafl-qemu.h
+++ xen/arch/arm/include/asm/libafl-qemu.h
@@ -7,8 +7,10 @@
 
 #include <xen/macros.h>
 
+#define LIBAFL_CALLING_CONVENTION noinline
+
 #define LIBAFL_DEFINE_FUNCTIONS(name, opcode)                           \
-    libafl_word _libafl_##name##_call0(                                 \
+    libafl_word LIBAFL_CALLING_CONVENTION _libafl_##name##_call0(       \
         libafl_word action) {                                           \
         register unsigned long r0 ASM_REG(0) = action;                  \
         __asm__ volatile (                                              \
@@ -20,7 +22,7 @@
         return r0;                                                      \
     }                                                                   \
                                                                         \
-    libafl_word _libafl_##name##_call1(                                 \
+    libafl_word LIBAFL_CALLING_CONVENTION _libafl_##name##_call1(       \
         libafl_word action, libafl_word arg1) {                         \
         register unsigned long r0 ASM_REG(0) = action;                  \
         register unsigned long r1 ASM_REG(1) = arg1;                    \
@@ -33,7 +35,7 @@
         return r0;                                                      \
     }                                                                   \
                                                                         \
-    libafl_word _libafl_##name##_call2(                                 \
+    libafl_word LIBAFL_CALLING_CONVENTION _libafl_##name##_call2(       \
         libafl_word action, libafl_word arg1, libafl_word arg2) {       \
         register unsigned long r0 ASM_REG(0) = action;                  \
         register unsigned long r1 ASM_REG(1) = arg1;                    \
