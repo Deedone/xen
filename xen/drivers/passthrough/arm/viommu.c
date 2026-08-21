Deedone/xen

commit 8b8a48d52151b02551fd28fd0fa091719f789c99

xen/arm: vsmmuv3: Add dummy support for virtual SMMUv3 for guests
domain_viommu_init() will be called during domain creation and will add
the dummy trap handler for virtual IOMMUs for guests.

A host IOMMU list will be created when host IOMMU devices are probed
and this list will be used to create the IOMMU device tree node for
dom0. For dom0, 1-1 mapping will be established between vIOMMU in dom0
and physical IOMMU.

For domUs, the 1-N mapping will be established between domU and physical
IOMMUs. A new area has been reserved in the arm guest physical map at
which the emulated vIOMMU node is created in the device tree.

Also set the vIOMMU type to vSMMUv3 to enable vIOMMU framework to call
vSMMUv3 domain creation/destroy functions.

Signed-off-by: Rahul Singh <rahul.singh@arm.com>
Signed-off-by: Milan Djokic <milan_djokic@epam.com>

xen/drivers/passthrough/arm/viommu.c
@@ -2,12 +2,44 @@
 
 #include <xen/errno.h>
 #include <xen/init.h>
+#include <xen/irq.h>
 #include <xen/types.h>
+#include <xen/sched.h>
 
 #include <asm/viommu.h>
 
+/* List of all host IOMMUs */
+LIST_HEAD(host_iommu_list);
+
 /* Struct to hold the vIOMMU ops and vIOMMU type */
 static const struct viommu_desc __ro_after_init *cur_viommu;
+
+/* Common function for adding to host_iommu_list */
+void add_to_host_iommu_list(paddr_t addr, paddr_t size,
+                            const struct dt_device_node *node)
+{
+    struct host_iommu *iommu_data;
+
+    iommu_data = xzalloc(struct host_iommu);
+    if ( !iommu_data )
+        panic("vIOMMU: Cannot allocate memory for host IOMMU data\n");
+
+    iommu_data->addr = addr;
+    iommu_data->size = size;
+    iommu_data->dt_node = node;
+    iommu_data->irq = platform_get_irq(node, 0);
+    if ( iommu_data->irq < 0 )
+    {
+        gdprintk(XENLOG_ERR,
+                 "vIOMMU: Cannot find a valid IOMMU irq\n");
+        xfree(iommu_data);
+        return;
+    }
+
+    printk("vIOMMU: Found IOMMU @0x%"PRIx64"\n", addr);
+
+    list_add_tail(&iommu_data->entry, &host_iommu_list);
+}
 
 int domain_viommu_init(struct domain *d, uint8_t viommu_type)
 {
@@ -37,6 +69,44 @@ uint8_t viommu_get_type(void)
         return XEN_DOMCTL_CONFIG_VIOMMU_NONE;
 
     return cur_viommu->viommu_type;
+}
+
+void set_cur_viommu(const struct viommu_desc *desc)
+{
+    if ( cur_viommu && (cur_viommu != desc) )
+    {
+        printk("WARNING: Cannot set vIOMMU, already set to a different value\n");
+        return;
+    }
+
+    cur_viommu = desc;
+}
+
+unsigned int domain_viommu_get_num_mmio_handlers(struct domain *d)
+{
+    if ( is_hardware_domain(d) )
+    {
+        struct host_iommu *hw_iommu;
+        int count = 0;
+
+        /*
+        * For hardware domain, vIOMMU per pIOMMU is exposed. Number of
+        * regions required for the configuration space is equal to number of
+        * host IOMMUs.
+        */
+        list_for_each_entry(hw_iommu, &host_iommu_list, entry)
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    /*
+     * For unpriviliged domains, single vIOMMU is exposed and requires one
+     * region to cover the configuration space.
+     */
+    return 1;
 }
 
 /*

xen/arch/arm/domain.c
@@ -678,7 +678,8 @@ int arch_sanitise_domain_config(struct xen_domctl_createdomain *config)
         return -EINVAL;
     }
 
-    if ( config->arch.viommu_type != XEN_DOMCTL_CONFIG_VIOMMU_NONE )
+    if ( config->arch.viommu_type != XEN_DOMCTL_CONFIG_VIOMMU_NONE &&
+         config->arch.viommu_type != viommu_get_type() )
     {
         dprintk(XENLOG_INFO,
                 "vIOMMU type requested not supported by the platform or Xen\n");
@@ -733,6 +734,8 @@ int arch_domain_create(struct domain *d,
         goto fail;
 
     count += domain_vpci_get_num_mmio_handlers(d);
+
+    count += domain_viommu_get_num_mmio_handlers(d);
 
     if ( (rc = domain_io_init(d, count + MAX_IO_HANDLER)) != 0 )
         goto fail;

xen/arch/arm/include/asm/domain.h
@@ -102,6 +102,11 @@ struct arch_domain
 #ifdef CONFIG_MPU
     uint8_t v8r_el1_msa;
 #endif
+
+#ifdef CONFIG_ARM_VIRTUAL_IOMMU
+    struct list_head viommu_list;     /* List of virtual IOMMUs */
+#endif
+
 }  __cacheline_aligned;
 
 struct arch_vcpu

xen/arch/arm/include/asm/viommu.h
@@ -5,8 +5,20 @@
 #ifdef CONFIG_ARM_VIRTUAL_IOMMU
 
 #include <xen/lib.h>
+#include <xen/list.h>
 #include <xen/types.h>
 #include <public/xen.h>
+
+extern struct list_head host_iommu_list;
+
+/* data structure for each hardware IOMMU */
+struct host_iommu {
+    struct list_head entry;
+    const struct dt_device_node *dt_node;
+    paddr_t addr;
+    paddr_t size;
+    uint32_t irq;
+};
 
 struct viommu_ops {
     /*
@@ -35,6 +47,10 @@ struct viommu_desc {
 int domain_viommu_init(struct domain *d, uint8_t viommu_type);
 int viommu_relinquish_resources(struct domain *d);
 uint8_t viommu_get_type(void);
+void add_to_host_iommu_list(paddr_t addr, paddr_t size,
+                            const struct dt_device_node *node);
+void set_cur_viommu(const struct viommu_desc *desc);
+unsigned int domain_viommu_get_num_mmio_handlers(struct domain *d);
 
 #else
 
@@ -52,6 +68,17 @@ static inline int domain_viommu_init(struct domain *d, uint16_t viommu_type)
 }
 
 static inline int viommu_relinquish_resources(struct domain *d)
+{
+    return 0;
+}
+
+static inline void add_to_host_iommu_list(paddr_t addr, paddr_t size,
+                                          const struct dt_device_node *node)
+{
+    return;
+}
+
+static inline unsigned int domain_viommu_get_num_mmio_handlers(struct domain *d)
 {
     return 0;
 }

xen/drivers/passthrough/Kconfig
@@ -40,6 +40,14 @@ config ARM_VIRTUAL_IOMMU
 	help
 	  Support virtual IOMMU infrastructure to implement vIOMMU.
 
+config VIRTUAL_ARM_SMMU_V3
+	bool "ARM Ltd. Virtual SMMUv3 Support (UNSUPPORTED)"
+	depends on ARM_SMMU_V3 && ARM_VIRTUAL_IOMMU
+	help
+	 Support for implementations of the virtual ARM System MMU architecture
+	 version 3. Virtual SMMUv3 is unsupported feature and should not be used
+	 in production.
+
 endif
 
 config AMD_IOMMU

xen/drivers/passthrough/arm/Makefile
@@ -3,3 +3,4 @@ obj-$(CONFIG_ARM_SMMU) += smmu.o
 obj-$(CONFIG_IPMMU_VMSA) += ipmmu-vmsa.o
 obj-$(CONFIG_ARM_SMMU_V3) += smmu-v3.o
 obj-$(CONFIG_ARM_VIRTUAL_IOMMU) += viommu.o
+obj-$(CONFIG_VIRTUAL_ARM_SMMU_V3) += vsmmu-v3.o

xen/drivers/passthrough/arm/smmu-v3.c
@@ -94,6 +94,7 @@
 #include <asm/suspend.h>
 
 #include "smmu-v3.h"
+#include "vsmmu-v3.h"
 
 #ifdef CONFIG_SYSTEM_SUSPEND
 #define __init_or_smmu_suspend
@@ -2844,6 +2845,9 @@ static int __init arm_smmu_device_probe(struct platform_device *pdev)
 	list_add(&smmu->devices, &arm_smmu_devices);
 	spin_unlock(&arm_smmu_devices_lock);
 
+	/* Add to host IOMMU list to initialize vIOMMU for dom0 */
+	add_to_host_iommu_list(ioaddr, iosize, dev_to_dt(pdev));
+
 	return 0;
 
 out_free_irqs:
@@ -3280,6 +3284,9 @@ static __init int arm_smmu_dt_init(struct dt_device_node *dev,
 	BUG_ON(!smmu);
 
 	platform_features &= smmu->features;
+
+	/* Set vIOMMU type to SMMUv3 */
+	vsmmuv3_set_type();
 
 	return 0;
 }

xen/drivers/passthrough/arm/vsmmu-v3.c
@@ -1,0 +1,116 @@
+/* SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause) */
+
+#include <xen/param.h>
+#include <xen/sched.h>
+#include <asm/mmio.h>
+#include <asm/viommu.h>
+
+struct virt_smmu {
+    struct      domain *d;
+    struct      list_head viommu_list;
+};
+
+static int vsmmuv3_mmio_write(struct vcpu *v, mmio_info_t *info,
+                              register_t r, void *priv)
+{
+    return IO_HANDLED;
+}
+
+static int vsmmuv3_mmio_read(struct vcpu *v, mmio_info_t *info,
+                             register_t *r, void *priv)
+{
+    return IO_HANDLED;
+}
+
+static const struct mmio_handler_ops vsmmuv3_mmio_handler = {
+    .read  = vsmmuv3_mmio_read,
+    .write = vsmmuv3_mmio_write,
+};
+
+static int vsmmuv3_init_single(struct domain *d, paddr_t addr, paddr_t size)
+{
+    struct virt_smmu *smmu;
+
+    smmu = xzalloc(struct virt_smmu);
+    if ( !smmu )
+        return -ENOMEM;
+
+    smmu->d = d;
+
+    register_mmio_handler(d, &vsmmuv3_mmio_handler, addr, size, smmu);
+
+    /* Register the vIOMMU to be able to clean it up later. */
+    list_add_tail(&smmu->viommu_list, &d->arch.viommu_list);
+
+    return 0;
+}
+
+int domain_vsmmuv3_init(struct domain *d)
+{
+    int ret;
+
+    INIT_LIST_HEAD(&d->arch.viommu_list);
+
+    if ( is_hardware_domain(d) )
+    {
+        struct host_iommu *hw_iommu;
+
+        list_for_each_entry(hw_iommu, &host_iommu_list, entry)
+        {
+            ret = vsmmuv3_init_single(d, hw_iommu->addr, hw_iommu->size);
+            if ( ret )
+                return ret;
+        }
+    }
+    else
+    {
+        ret = vsmmuv3_init_single(d, GUEST_VSMMUV3_BASE, GUEST_VSMMUV3_SIZE);
+        if ( ret )
+            return ret;
+    }
+
+    return 0;
+}
+
+int vsmmuv3_relinquish_resources(struct domain *d)
+{
+    struct virt_smmu *pos, *temp;
+
+    /* Cope with uninitialized vIOMMU */
+    if ( list_head_is_null(&d->arch.viommu_list) )
+        return 0;
+
+    list_for_each_entry_safe(pos, temp, &d->arch.viommu_list, viommu_list )
+    {
+        list_del(&pos->viommu_list);
+        xfree(pos);
+    }
+
+    return 0;
+}
+
+static const struct viommu_ops vsmmuv3_ops = {
+    .domain_init = domain_vsmmuv3_init,
+    .relinquish_resources = vsmmuv3_relinquish_resources,
+};
+
+static const struct viommu_desc vsmmuv3_desc = {
+    .ops = &vsmmuv3_ops,
+    .viommu_type = XEN_DOMCTL_CONFIG_VIOMMU_SMMUV3,
+};
+
+void __init vsmmuv3_set_type(void)
+{
+    const struct viommu_desc *desc = &vsmmuv3_desc;
+
+    set_cur_viommu(desc);
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

commit c0ef00e581af86e8436d0fa08737c429c554bf04

xen/arm: fix host IOMMU IRQ validation
platform_get_irq() returns a signed error, but host_iommu.irq is
unsigned. Storing the result before checking it converts a missing IRQ
to UINT_MAX, so hardware-domain creation tries to reserve vIRQ
4294967295 and fails.

Validate the signed result before storing it. Disable guest vIOMMU
support when an SMMUv3 cannot provide the IRQ required by the current
virtual interface. This avoids exposing an incomplete SMMUv3 topology.

Physical IOMMU support remains enabled on IRQ-less platforms.

Fixes: 8b8a48d52151 ("xen/arm: vsmmuv3: Add dummy support for virtual SMMUv3 for guests")
Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

xen/drivers/passthrough/arm/viommu.c
@@ -13,6 +13,7 @@ LIST_HEAD(host_iommu_list);
 
 /* Struct to hold the vIOMMU ops and vIOMMU type */
 static const struct viommu_desc __ro_after_init *cur_viommu;
+static bool host_iommu_list_incomplete;
 
 /* Common function for adding to host_iommu_list */
 void add_to_host_iommu_list(paddr_t addr, paddr_t size,
@@ -20,6 +21,7 @@ void add_to_host_iommu_list(paddr_t addr, paddr_t size,
                             uint32_t features)
 {
     struct host_iommu *iommu_data;
+    int irq;
 
     iommu_data = xzalloc(struct host_iommu);
     if ( !iommu_data )
@@ -29,14 +31,17 @@ void add_to_host_iommu_list(paddr_t addr, paddr_t size,
     iommu_data->size = size;
     iommu_data->dt_node = node;
     iommu_data->features = features;
-    iommu_data->irq = platform_get_irq(node, 0);
-    if ( iommu_data->irq < 0 )
+    irq = platform_get_irq(node, 0);
+    if ( irq < 0 )
     {
-        gdprintk(XENLOG_ERR,
-                 "vIOMMU: Cannot find a valid IOMMU irq\n");
+        host_iommu_list_incomplete = true;
+        printk(XENLOG_WARNING
+               "vIOMMU: disabling support because IOMMU @0x%"PRIx64
+               " has no usable IRQ (%d)\n", addr, irq);
         xfree(iommu_data);
         return;
     }
+    iommu_data->irq = irq;
 
     printk("vIOMMU: Found IOMMU @0x%"PRIx64"\n", addr);
 
@@ -74,7 +79,8 @@ int viommu_allocate_free_vid(struct domain *d, uint32_t id, uint32_t *vid) {
 
 uint8_t viommu_get_type(void)
 {
-    if ( !cur_viommu )
+    if ( !cur_viommu || host_iommu_list_incomplete ||
+         list_empty(&host_iommu_list) )
         return XEN_DOMCTL_CONFIG_VIOMMU_NONE;
 
     return cur_viommu->viommu_type;