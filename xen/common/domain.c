2406728fcff0e4d9df41b87fcf24b9bf92ee2988
Stewart Hildebrand <stewart.hildebrand@amd.com>
2026-09-07 14:02:20 +0300

    vpci: allow queueing of mapping operations
    Introduce vPCI BAR mapping task queue. Store information needed to
    map/unmap BARs in struct vpci_map_task. Allow queueing of BAR map/unmap
    operations in a list, thus making it possible to perform multiple p2m
    operations associated with single PCI config space register access.
    
    The rangesets describing the ranges to be (un)mapped move from struct
    vpci_bar into the task. They are only live for the duration of the
    operation, and a single per-device set cannot describe more than one
    queued operation. Allocate them along with the task and free them once
    the task has been processed.
    
    This is preparatory work for further changes that need to perform
    multiple unmap/map operations before returning to guest.
    
    At the moment, only a single operation will be queued. However, when
    multiple operations are queued, there is a check in modify_bars() to
    skip BARs already in the requested state that will no longer be
    accurate. Remove this check in preparation of upcoming changes.
    
    Amends: 622bdd962822 ("vpci/header: handle p2m range sets per BAR")
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Message-ID: <20260406191203.97662-3-stewart.hildebrand@amd.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    ---
    v3->v4:
    * drop "vpci: Use pervcpu ranges for BAR mapping"; the rangesets are
      now allocated per task here rather than per vcpu, so that patch only
      added code this one removed again
    * non-functional churn split out into "vpci: use a local variable for
      the BAR rangeset"
    
    v2->v3:
    * new patch in this series, borrowed from [1]
    
    [1]: https://patchew.org/Xen/20260406191203.97662-1-stewart.hildebrand@amd.com/20260406191203.97662-3-stewart.hildebrand@amd.com/

--- xen/common/domain.c
+++ xen/common/domain.c
@@ -508,6 +508,8 @@ struct vcpu *vcpu_create(struct domain *d, unsigned int vcpu_id)
     if ( arch_vcpu_create(v) != 0 )
         goto fail_sched;
 
+    vpci_vcpu_init(v);
+
     d->vcpu[vcpu_id] = v;
     if ( vcpu_id != 0 )
     {
